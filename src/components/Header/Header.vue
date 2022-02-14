7e9b206195e76f3fbab95b6b9594a6b91569d0dc
Phuong Ta <phuong.ta@ipos.vn>
2022-02-15 00:19:55 +0700

    Task1: Make header
    Task1: Make header

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<nav class="navbar" role="navigation" aria-label="main navigation">
+			<div class="navbar-brand"></div>
+		</nav>
+		<div class="navbar-end is-hidden-mobile">
+			<Menu></Menu>
+		</div>
+	</div>
+</template>
+
+<script>
+	import Menu from './Menu.vue';
+	export default {
+		name: 'Header',
+		data() {
+			return {};
+		},
+		components: {
+			Menu,
+		},
+		computed: {},
+		methods: {},
+	};
+</script>
+
+<style scoped></style>
