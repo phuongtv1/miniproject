263b03bea94c9458a4ba6cef4a37387a9f3edf64
Phuong Ta <phuong.ta@ipos.vn>
2022-02-20 18:53:45 +0000

    [main] Task1

---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="footer">
+        <div class="columns container">
+            <div class="column">
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit..</p>
+            </div>
+            <div class="column has-text-right">
+                <p>Vuemerce | Made in VN</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Footer'
+}
+</script>
+<style scoped>
+.footer {
+    background: #b9e2fc;
+    color: black;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="home">
     <Header />
+    <Products />
+    <Footer />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Header from "@/components/Header/Header.vue";
+import Footer from "@/components/Footer/Footer.vue";
+import Products from "@/components/Product/Products.vue";
 
 export default {
   name: "Home",
   components: {
     Header,
+    Footer,
+    Products
   },
 };
 </script>
